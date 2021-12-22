1744ee4fceca8e3114d0234dc518ff4c4d80f212
Lukasz Kot <lukasz.kot@hotmail.com>
2021-12-22 13:54:24 +0100

    Basic Header component implemented.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
-  return (
-    <div>
-      This is initial markup.
-    </div>
-  );
+	return (
+		<div>
+			<Header></Header>
+		</div>
+	);
 }
 
 export default App;
