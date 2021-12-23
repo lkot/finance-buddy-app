2ac20818b5fd7c4055ec92dc3240e4481c03f2cf
Lukasz Kot <lukasz.kot@hotmail.com>
2021-12-23 10:45:28 +0100

    AddTransaction component added + minor code refactoring.

--- src/components/Finances.js
+++ src/components/Finances.js
@@ -2,20 +2,20 @@ import React from 'react';
 
 function Finances() {
 	return (
-			<div class='inc-exp-container'>
-				<div>
-					<h4>Income</h4>
-					<p id='money-plus' class='money plus'>
-						+$0.00
-					</p>
-				</div>
-				<div>
-					<h4>Expense</h4>
-					<p id='money-minus' class='money minus'>
-						-$0.00
-					</p>
-				</div>
+		<div class='inc-exp-container'>
+			<div>
+				<h4>Income</h4>
+				<p className='money plus'>
+					+$0.00
+				</p>
 			</div>
+			<div>
+				<h4>Expense</h4>
+				<p className='money minus'>
+					-$0.00
+				</p>
+			</div>
+		</div>
 	);
 }
 
