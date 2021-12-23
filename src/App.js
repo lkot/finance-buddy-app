2f0e37ef3925851f1db739302c6cfafeb9b4ce77
Lukasz Kot <lukasz.kot@hotmail.com>
2021-12-23 10:14:01 +0100

    TransactionsList component added to the project.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Balance from './components/Balance';
 import Finances from './components/Finances';
+import TransactionsList from './components/TransactionsList';
 
 function App() {
 	return (
@@ -11,6 +12,7 @@ function App() {
 			<div className='container'>
 				<Balance />
 				<Finances />
+				<TransactionsList />
 			</div>
 		</div>
 	);
---
+++ src/components/TransactionsList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function TransactionsList() {
+	return (
+		<>
+			<h3>History</h3>
+			<ul id='list' class='list'>
+				<li class='minus'>
+					Cash <span>-$400</span>
+					<button class='delete-btn'>x</button>
+				</li>
+			</ul>
+		</>
+	);
+}
+
+export default TransactionsList;
