ff529a86fe8c1b14386146df85deabba36541387
Lukasz Kot <lukasz.kot@hotmail.com>
2021-12-28 11:41:00 +0100

    Transaction's final stylization.

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 export const Transaction = ({ transaction }) => {
-	const sign = transaction.amout < 0 ? '-' : '+';
+	const sign = transaction.amount < 0 ? '-' : '+';
 
 	return (
-		<li className='minus'>
+		<li className={transaction.amount < 0 ? 'minus' : 'plus'}>
 			{transaction.text}{' '}
 			<span>
-				{sign}${transaction.amount}
+				{sign}${Math.abs(transaction.amount)}
 			</span>
 			<button className='delete-btn'>x</button>
 		</li>
