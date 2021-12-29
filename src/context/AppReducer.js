2371ebd7c638c2de93e9805c06ec5754c4a82e14
Lukasz Kot <lukasz.kot@hotmail.com>
2021-12-29 11:00:40 +0100

    Implementing delete transaction using AppReducer.

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,6 +1,14 @@
-export default (state,action) => {
-    switch(action.type) {
-        default:
-            return state;
-    }
-}
+export default (state, action) => {
+	// Reducer is just the way to change the state and send it down to the component/appliation.
+	switch (action.type) {
+		case 'DELETE_TRANSACTION':
+			return {
+				...state,
+				transactions: state.transactions.filter(
+					(transaction) => transaction.id !== action.payload
+				),
+			};
+		default:
+			return state;
+	}
+};
